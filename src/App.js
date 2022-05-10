9121e25cc86e4f5be356db432889bc0218e10aee
Titus Lim <titus.happy@gmail.com>
2022-05-10 14:41:41 +0800

    Navigation tabs complete

--- src/App.js
+++ src/App.js
@@ -24,10 +24,40 @@ function App() {
 
         {/* Content (Right) */}
         <div class="col-span-2 rounded-r-md grow bg-gray-600 h-full w-2/3 p-4 flex flex-col">
-          <h5>Your favourite sites</h5>
-          <p class="flex-1">
-            Lorem Ipsum
-          </p>
+          <h5 class="text-center">Your favourite sites</h5>
+          {/* Tabs and Routes */}
+          <div class="w-full h-full flex-1">
+            <div class="pb-2">
+              <ul class="flex flex-wrap -mb-px text-sm font-normal text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                <li class="mr-2" role="presentation">
+                  <button class="inline-block p-2 rounded-t-lg border-b-2 border-transparent outline-none hover:text-white-800 hover:bg-white-100/10 text-white-400 active:bg-white-100/10" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
+                </li>
+                <li class="mr-2" role="presentation">
+                  <button class="inline-block p-2 rounded-t-lg border-b-2 border-transparent outline-none hover:text-white-800 hover:bg-white-100/10 text-white-400" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Dashboard</button>
+                </li>
+                <li class="mr-2" role="presentation">
+                  <button class="inline-block p-2 rounded-t-lg border-b-2 border-transparent outline-none hover:text-white-800 hover:bg-white-100/10 text-white-400" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
+                </li>
+                <li role="presentation">
+                  <button class="inline-block p-2 rounded-t-lg border-b-2 border-transparent outline-none hover:text-white-800 hover:bg-white-100/10 text-white-400" id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
+                </li>
+              </ul>
+            </div>
+            <div id="myTabContent">
+              <div class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+              </div>
+              <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+              </div>
+              <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+              </div>
+              <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
+                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
