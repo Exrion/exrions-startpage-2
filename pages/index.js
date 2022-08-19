cc5244b783c7b7eaf7c87d3d7be6d1749b229617
Titus Lim <titus.happy@gmail.com>
2022-08-19 12:17:19 +0800

    Added footer

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/_layout';
 import Link from 'next/link';
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <>
       <Layout home>
-
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
       </Layout>
     </>
   );
