b4e66dda9003c88b35de79362f06c9e1344a7cdd
Exrion - Titus Lim <titus.happy@gmail.com>
2023-01-19 17:48:03 +0800

    Fixed API request

--- src/Pages/Startpage/Background.component.tsx
+++ src/Pages/Startpage/Background.component.tsx
@@ -7,27 +7,30 @@ export default function BackgroundComponent({ children }: { children: React.Reac
         const orientation = 'landscape';
         const collectionId = '11649432';
 
-        const apiUrl = `${unsplashURL}/photos/random?collections=${collectionId}&orientation=${orientation}`;
+        const apiUrl = `${unsplashURL}/photos/random`;
         const apiConfig = {
             headers: {
-                'Authorization': process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
+                'Authorization': `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
                 'Accept-Version': 'v1'
+            },
+            params: {
+                'collections': collectionId,
+                'orientation': orientation
             }
         }
 
-        let img: any;
+        let imgUrl: any;
         axios.get(
             apiUrl,
-            apiConfig
+            // apiConfig
         ).then(res => {
-            console.log(res);
-            img = JSON.parse(res.data)['urls']['full'];
+            imgUrl = res.data['urls']['full'];
         })
             .catch(e => {
                 console.log(e);
             })
             .finally(() => {
-                document.body.style.backgroundImage = `url(${img}) no-repeat center center fixed`;
+                document.body.style.backgroundImage = `url(${imgUrl}) no-repeat center fixed`;
             });
     };
 
