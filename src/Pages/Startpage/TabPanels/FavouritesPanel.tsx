b3f176e73639b45af61c23f5003df561084bc4ee
Titus Lim <titus.happy@gmail.com>
2023-01-26 17:29:59 +0800

    minor update

--- src/Pages/Startpage/TabPanels/FavouritesPanel.tsx
+++ src/Pages/Startpage/TabPanels/FavouritesPanel.tsx
@@ -9,7 +9,7 @@ export default function FavouritesPanel() {
             >
                 {/* Left Column */}
                 <Box>
-
+                    
                 </Box>
 
                 {/* Right Column */}
