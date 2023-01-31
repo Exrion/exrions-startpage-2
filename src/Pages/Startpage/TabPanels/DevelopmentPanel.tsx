5d0ca8504b3374a911dc3f6fa3e6c88dab9e8d5c
Exrion - Titus Lim <titus.happy@gmail.com>
2023-01-31 11:12:58 +0800

    Added links

--- src/Pages/Startpage/TabPanels/DevelopmentPanel.tsx
+++ src/Pages/Startpage/TabPanels/DevelopmentPanel.tsx
@@ -1,4 +1,7 @@
+import { mdiGithub, mdiMicrosoftAzureDevops, mdiMaterialUi, mdiTailwind, mdiServer, mdiApi } from "@mdi/js";
+import Icon from "@mdi/react";
 import { Box, Stack, Typography } from "@mui/material";
+import { LinkDevelopment_DevOps, LinkDevelopment_GitHub, LinkDevelopment_MUI, LinkDevelopment_Tailwind, LinkDevelopment_Unsplash, LinkDevelopment_Vercel } from "../../../Data/Constants/Portfolio/TabPanelLinks";
 import Link from "../Links/Link";
 
 export default function DevelopmentPanel() {
@@ -13,17 +16,18 @@ export default function DevelopmentPanel() {
                     {/* Left Column */}
                     <Box width={{ md: 0.5, sm: 0.5, xs: 1 }}>
                         <Stack spacing={1}>
-                            <Link />
-                            <Link />
-                            <Link />
+                            <Link label='GitHub' link={LinkDevelopment_GitHub} icon={<Icon path={mdiGithub} size={1} />} />
+                            <Link label='Azure DevOps' link={LinkDevelopment_DevOps} icon={<Icon path={mdiMicrosoftAzureDevops} size={1} />} />
+                            <Link label='Material UI Docs' link={LinkDevelopment_MUI} icon={<Icon path={mdiMaterialUi} size={1} />} />
                         </Stack>
                     </Box>
 
                     {/* Right Column */}
                     <Box width={{ md: 0.5, sm: 0.5, xs: 1 }}>
                         <Stack spacing={1}>
-                            <Link />
-                            <Link />
+                            <Link label='Tailwind CSS Docs' link={LinkDevelopment_Tailwind} icon={<Icon path={mdiTailwind} size={1} />} />
+                            <Link label='Vercel' link={LinkDevelopment_Vercel} icon={<Icon path={mdiServer} size={1} />} />
+                            <Link label='Unsplash API' link={LinkDevelopment_Unsplash} icon={<Icon path={mdiApi} size={1} />} />
                         </Stack>
                     </Box>
                 </Stack>
--- src/Pages/Startpage/TabPanels/SchoolPanel.tsx
+++ src/Pages/Startpage/TabPanels/SchoolPanel.tsx
@@ -1,4 +1,8 @@
-import { Box, Link, Stack, Typography } from "@mui/material";
+import { mdiSchool, mdiAccountSchoolOutline, mdiNoteMultiple, mdiServerNetwork, mdiPalmTree, mdiShopping } from "@mdi/js";
+import Icon from "@mdi/react";
+import { Box, Stack, Typography } from "@mui/material";
+import { LinkSchool_LEO2External, LinkSchool_StudentPortal, LinkSchool_LEO2Internal, LinkSchool_SA2, LinkSchool_Intranet, LinkSchool_Oasis, LinkSchool_POLITE } from "../../../Data/Constants/Portfolio/TabPanelLinks";
+import Link from "../Links/Link";
 
 export default function SchoolPanel() {
     return (
@@ -12,17 +16,19 @@ export default function SchoolPanel() {
                     {/* Left Column */}
                     <Box width={{ md: 0.5, sm: 0.5, xs: 1 }}>
                         <Stack spacing={1}>
-                            <Link />
-                            <Link />
-                            <Link />
+                            <Link label='LEO 2.0 (External)' link={LinkSchool_LEO2External} icon={<Icon path={mdiSchool} size={1} />} />
+                            <Link label='RP Student Portal' link={LinkSchool_StudentPortal} icon={<Icon path={mdiAccountSchoolOutline} size={1} />} />
+                            <Link label='Oasis' link={LinkSchool_Oasis} icon={<Icon path={mdiPalmTree} size={1} />} />
+                            <Link label='POLITE Mall' link={LinkSchool_POLITE} icon={<Icon path={mdiShopping} size={1} />} />
                         </Stack>
                     </Box>
 
                     {/* Right Column */}
                     <Box width={{ md: 0.5, sm: 0.5, xs: 1 }}>
                         <Stack spacing={1}>
-                            <Link />
-                            <Link />
+                            <Link label='LEO 2.0 (Internal)' link={LinkSchool_LEO2Internal} icon={<Icon path={mdiSchool} size={1} />} />
+                            <Link label='SA 2.0' link={LinkSchool_SA2} icon={<Icon path={mdiNoteMultiple} size={1} />} />
+                            <Link label='Intranet' link={LinkSchool_Intranet} icon={<Icon path={mdiServerNetwork} size={1} />} />
                         </Stack>
                     </Box>
                 </Stack>
