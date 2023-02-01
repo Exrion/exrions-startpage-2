fae97a15644d03e09483c27d6374d1a13e11a65e
Titus Lim <titus.happy@gmail.com>
2023-02-01 15:18:09 +0800

    added fonts

--- src/Styles/Themes/RootThemes.tsx
+++ src/Styles/Themes/RootThemes.tsx
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material";
+import RubikTtf from '../../Data/Fonts/Rubik/Rubik-Medium.ttf';
 
 export const startpageTheme = createTheme({
   palette: {
@@ -43,5 +44,31 @@ export const startpageTheme = createTheme({
 });
 
 export const portfolioTheme = createTheme({
-
+  typography: {
+    h1: { fontSize: '5rem' },
+    fontFamily: 'Rubik, sans-serif',
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 425,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Raleway';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('Rubik'), local('Rubik-Medium'), url(${RubikTtf}) format('ttf');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  }
 });
