1fd56c845b5380ca49602a8d9f9a75d9c0700621
Exrion - Titus Lim <titus.happy@gmail.com>
2023-02-03 17:10:13 +0800

    added animation

--- src/Pages/Portfolio/Footer/FooterLayout.component.tsx
+++ src/Pages/Portfolio/Footer/FooterLayout.component.tsx
@@ -1,4 +1,4 @@
-import { BottomNavigation, Box, Stack, Typography } from "@mui/material";
+import { BottomNavigation, Box, Slide, Stack, Typography } from "@mui/material";
 import { officialName } from "../../../Data/Constants/MainConstants";
 import { FooterAnchorStyle, FooterBoxSx } from "../../../Styles/Elements/Portfolio/PortfolioRootStyles";
 import SectionContainer from "../Shared/SectionContainer.component";
@@ -7,39 +7,45 @@ export default function FooterLayout() {
     return (
         <>
             <SectionContainer>
-                <Box
-                    sx={FooterBoxSx}
-                    boxShadow={3}
+                <Slide
+                    in={true}
+                    timeout={2000}
+                    direction='up'
                 >
-                    <BottomNavigation>
-                        <Stack>
-                            <Typography
-                                fontFamily='monospace'
-                                variant='subtitle1'
-                                fontWeight={600}
-                                textAlign='center'
-                            >
-                                Designed and Developed by {officialName}
-                            </Typography>
-                            <Typography
-                                fontFamily='monospace'
-                                variant='subtitle2'
-                                fontWeight={600}
-                                color='GrayText'
-                                sx={{ textDecoration: 'underline' }}
-                                textAlign='center'
-                            >
-                                <a
-                                    href='https://github.com/Exrion/exrions-startpage-2'
-                                    style={FooterAnchorStyle}
-                                    tabIndex={0}
+                    <Box
+                        sx={FooterBoxSx}
+                        boxShadow={3}
+                    >
+                        <BottomNavigation>
+                            <Stack>
+                                <Typography
+                                    fontFamily='monospace'
+                                    variant='subtitle1'
+                                    fontWeight={600}
+                                    textAlign='center'
                                 >
-                                    View Source
-                                </a>
-                            </Typography>
-                        </Stack>
-                    </BottomNavigation>
-                </Box>
+                                    Designed and Developed by {officialName}
+                                </Typography>
+                                <Typography
+                                    fontFamily='monospace'
+                                    variant='subtitle2'
+                                    fontWeight={600}
+                                    color='GrayText'
+                                    sx={{ textDecoration: 'underline' }}
+                                    textAlign='center'
+                                >
+                                    <a
+                                        href='https://github.com/Exrion/exrions-startpage-2'
+                                        style={FooterAnchorStyle}
+                                        tabIndex={0}
+                                    >
+                                        View Source
+                                    </a>
+                                </Typography>
+                            </Stack>
+                        </BottomNavigation>
+                    </Box>
+                </Slide>
             </SectionContainer>
         </>
     );
